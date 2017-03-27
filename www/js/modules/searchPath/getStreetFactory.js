224e676de6a9454d9aa3c20b016f18be8deadab0
cgarcia0910 <cgarcia0910@gmail.com>
2017-03-28 00:17:09 +0200

    Get Street Factory modified to return only the nearest street segment

--- www/js/modules/searchPath/getStreetFactory.js
+++ www/js/modules/searchPath/getStreetFactory.js
@@ -6,27 +6,52 @@ function getStreetFactory ($q, $http) {
     interfaz.getStreet = function (lat, lng) {
         var defered = $q.defer();
         var promise = defered.promise;
+        var dtls = require('distance-to-line-segment');
         var utmObj = require('utm-latlng');
         var obj = new utmObj();
+        var utmClickPoint = {'x' : obj.convertLatLngToUtm(lat, lng).Easting, 'y': obj.convertLatLngToUtm(lat,lng).Northing};
+        console.log(utmClickPoint);
         $http( {
             method: 'POST',
             url: 'http://localhost:7474/db/data/ext/SpatialPlugin/graphdb/findClosestGeometries',
             data: '{"layer" : "streeMapNodeCorrect2Layer", "pointX" : '+lng+', "pointY" : '+lat+', "distanceInKm" : 0.0001}'
         }).then(function (data) {
                 var calles = {};
-                var latLng = [];
+                var streetLatLng = [];
+                var minDist = -1;
+                var streetSegment = [];
                 angular.forEach(data.data, function (key, value) {
                     angular.forEach(key.data.wkt.substring(12, key.data.wkt.length-1).split(", "), function (key, value) {
-                        latLng.push({lng : parseFloat(key.split(" ")[0]), lat: parseFloat(key.split(" ")[1])});
+                        streetLatLng.push({lng : parseFloat(key.split(" ")[0]), lat: parseFloat(key.split(" ")[1])});
                     });
-                    var utmStreet = [];
-                    angular.forEach(latLng, function (value, key) {
+                    var utmSegment = [];
+                    var utmPreviousPoint = "";//{'x' : 0, 'y': 0};
+                    var geoPreviousPoint = {};
+                    angular.forEach(streetLatLng, function (value, key) {
                         //var utmStreet.push({x: utm.convertLatLngToUtm(value.lat, value.lng).x, y: utm.convertLatLngToUtm(value.lat, value.lng).y});
-                        console.log('utm' + obj.convertLatLngToUtm(value.lat, value.lng).Easting);
-                        console.log(value);
+                        if (utmPreviousPoint === "") {
+                            utmPreviousPoint = {'x' : parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Easting), 'y': parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Northing)};
+                            geoPreviousPoint = {lat : value.lat, lng: value.lng};
+                        }
+                        else {
+                            if (minDist == -1 || minDist > dtls.squared(parseFloat(utmPreviousPoint.x), parseFloat(utmPreviousPoint.y), parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Easting), parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Northing), parseFloat(utmClickPoint.x), parseFloat(utmClickPoint.y))) {
+                                minDist = dtls.squared(parseFloat(utmPreviousPoint.x), parseFloat(utmPreviousPoint.y), parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Easting), parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Northing), parseFloat(utmClickPoint.x), parseFloat(utmClickPoint.y));
+                                console.log(geoPreviousPoint);
+                                streetSegment = [];
+                                streetSegment.push(geoPreviousPoint);
+                                //console.log({lng: (obj.convertUtmToLatLng(utmPreviousPoint.x).lng), lat: (obj.convertUtmToLatLng(utmPreviousPoint.x).lat)});
+                                //streetLatLng.push({lng: parseFloat(obj.convertUtmToLatLng(utmPreviousPoint.x).lng), lat: parseFloat(obj.convertUtmToLatLng(utmPreviousPoint.x).lat)});
+                                streetSegment.push({lng : parseFloat(value.lng), lat: parseFloat(value.lat)});
+                            }
+                            //console.log(dtls.squared(parseFloat(utmPreviousPoint.x), parseFloat(utmPreviousPoint.y), parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Easting), parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Northing), parseFloat(utmClickPoint.x), parseFloat(utmClickPoint.y)) );
+                            //utmPreviousPoint = {'x' : parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Easting), 'y': parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Northing)};
+                            utmPreviousPoint = {'x' : parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Easting), 'y': parseFloat(obj.convertLatLngToUtm(value.lat,value.lng).Northing)};
+                            geoPreviousPoint = {lat : value.lat, lng: value.lng};
+                        }
                     });
-                    calles["p"+value] = {color: '#008000', weight: 8, latlngs: latLng};
-                    latLng =[];
+
+                    calles["p"+value] = {color: '#008000', weight: 8, latlngs: streetSegment};
+                    streetLatLng =[];
                 });
                 console.log(calles);
                 defered.resolve(calles);
